f243890c1fb044f977a19b3421472c24f58efc00
Ben Crittenden <130415312+BenCritt@users.noreply.github.com>
2024-09-25 17:52:45 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -3,8 +3,11 @@ class DomainForm(forms.Form):
     # Define a form field for the domain input
     domain = forms.CharField(
         # Label that will be displayed with the input field
-        label="Domain Name:",
+        label="Enter Domain Name",
         # Maximum length of the input string
         max_length=253,
-        # help_text="Enter a valid domain name (e.g., example.com).",
+        widget=forms.TextInput(
+            # Placeholder text to provide an example of the expected input
+            attrs={"placeholder": "bencritt.net"}
+        ),
     )
